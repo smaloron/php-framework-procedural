6a920b1cb680a5a183f4f6fd5903acac3e0c3312
seb <contact@smaloron.eu>
2022-01-11 16:28:07 +0100

    Gestion des messages flash, signature des fonctions

---
+++ lib/flash.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Définition d'un message flash
+ *
+ * @param string $message
+ * @return void
+ */
+function addFlash(string $message): void {
+
+}
+
+/**
+ * Récupération du message flash
+ * et suppression de celui-ci dans la session
+ * @return string
+ */
+function getFlash(): string {
+
+}
+
+/**
+ * test de l'existence d'un message flash
+ *
+ * @return boolean
+ */
+function hasFlash(): bool {
+
+}
