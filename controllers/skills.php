e14206d82dd78ef1540e8df388027b3d69900568
seb <contact@smaloron.eu>
2022-01-12 13:34:44 +0100

    gestion des compétences avec lecture/écriture de fichier json

--- controllers/skills.php
+++ controllers/skills.php
@@ -1,12 +1,63 @@
 <?php
+
+$filter = filter_input(INPUT_GET, "startWith");
+
+$skillPath = "data/skills.json";
+
 // Lecture des données
-$skills = file_get_contents("data/skills.json");
+$skills = file_get_contents($skillPath);
 
 $skillsAsArray = json_decode($skills, true);
 
 $skillsAsObject = json_decode($skills);
 
+$isPosted = filter_has_var(INPUT_POST, "submit");
+
+if ($isPosted) {
+    $skillName = filter_input(INPUT_POST, "skill", FILTER_SANITIZE_STRING);
+
+    if (!empty($skillName)) {
+        $skillsAsArray[] = [
+            "label" => $skillName,
+            "code" => uniqid(),
+        ];
+        file_put_contents($skillPath, json_encode($skillsAsArray));
+
+        header("location:" . getLinkToRoute("skills"));
+    }
+}
+
+$skillsAsArray = array_map(
+    function ($item) {
+        $item["label"] = strtoupper($item["label"]);
+        return $item;
+    }, 
+    $skillsAsArray
+);
+
+// Liens vers les premières lettres des compétences
+
+$letters = array_map(
+    function($item){
+        return substr($item,0,1);
+    },
+    array_column($skillsAsArray, "label")
+);
+$letters = array_unique($labels);
+
+
+// Filtrage des données
+$skillsAsArray = array_filter(
+    $skillsAsArray,
+    function($item) use ($filter){
+        return str_starts_with($item["label"], $filter);
+    }
+);
+
+
+
 echo render("skills", [
     "skills" => $skillsAsArray,
-    "skillsObj" => $skillsAsObject
+    "skillsObj" => $skillsAsObject,
+    "letters" => $letters
 ]);
--- views/skills.php
+++ views/skills.php
@@ -1,3 +1,16 @@
+<div class="mb-4">
+    <form method="post">
+        <div class="mb-3">
+            <label class="form-label">Compétence</label>
+            <input type="text" class="form-control" name="skill">
+        </div>
+
+        <button class="btn btn-primary" type="submit" name="submit">
+            Valider
+        </button>
+    </form>
+</div>
+
 <ul>
     <?php foreach($skills as $item): ?>
         <li><?=$item["label"]?></li>
