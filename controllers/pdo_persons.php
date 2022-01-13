debe28fd68fd134b6e3a24478a4fbf309854f532
seb <contact@smaloron.eu>
2022-01-13 14:09:04 +0100

    Mise à jour des personnes

--- controllers/pdo_persons.php
+++ controllers/pdo_persons.php
@@ -10,11 +10,49 @@ $db = new PDO(
 $id = (int) filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 $action = filter_input(INPUT_GET, "action", FILTER_SANITIZE_STRING);
 
+$isPosted = filter_has_var(INPUT_POST, "first_name");
+
+// Traitement du formulaire
+if($isPosted){
+    // Récupération des données
+    $firstName = filter_input(INPUT_POST, "first_name", FILTER_SANITIZE_STRING);
+    $lastName = filter_input(INPUT_POST, "last_name", FILTER_SANITIZE_STRING);
+    $id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_NUMBER_INT);
+
+    if(!empty(trim($lastName)) && ! empty(trim($firstName))){
+        $queryParams = [$firstName, $lastName];
+
+        if(empty($id)){
+            $sql = "INSERT INTO persons (first_name, last_name) VALUES (?, ?)";
+        } else {
+            $sql = "UPDATE persons SET first_name=?, last_name=? WHERE id=?";
+            $queryParams[] = $id;
+        }
+        
+        $statement = $db->prepare($sql);
+        $statement->execute($queryParams);
+        header("Location: " . getLinkToRoute("pdo_persons"));
+    }
+}
+
 // Gestion de la suppression
 if($id && $action === "delete"){
     $sql = "DELETE FROM persons WHERE id=$id";
     $db->exec($sql);
     header("Location: ". getLinkToRoute("pdo_persons"));
+}
+
+// En cas de modification, récupération des infos
+// de la personne à modifier
+if($id && $action === "update"){
+    $sql = "SELECT * FROM persons WHERE id=$id";
+    $result = $db->query($sql);
+    $currentPerson = $result->fetch(PDO::FETCH_OBJ);
+} else {
+    $currentPerson = new StdClass();
+    $currentPerson->first_name = "";
+    $currentPerson->last_name = "";
+    $currentPerson->id = null;
 }
 
 // Requête pour lister toutes les personnes
@@ -24,7 +62,10 @@ $data = $result->fetchAll(PDO::FETCH_OBJ);
 
 
 // Affichage de la vue
-echo render("persons", ["personList" => $data]);
+echo render("persons", [
+    "personList" => $data,
+    "currentPerson" => $currentPerson
+]);
 
 
 /*
--- views/persons.php
+++ views/persons.php
@@ -1,9 +1,31 @@
 <h1>Liste des personnes </h1>
+
+<div class="mb-4 mt-3">
+    <h2>Ajout d'une personne</h2>
+    <form method="post" class="row p-4">
+        <div class="mb-3 col">
+            <label class="form-label">Prénom</label>
+            <input type="text" class="form-control" name="first_name"
+            value="<?= $currentPerson->first_name?>">
+        </div>
+        <div class="mb-3 col">
+            <label class="form-label">Nom</label>
+            <input type="text" class="form-control" name="last_name"
+            value="<?= $currentPerson->last_name?>">
+        </div>
+
+        <input type="hidden" name="id" value="<?= $currentPerson->id ?>">
+
+        <button type="submit" class="btn btn-primary">Valider</button>
+    </form>
+</div>
+
 <table class="table">
     <thead>
         <tr>
             <td>Prénom</td>
             <td>Nom</td>
+            <td></td>
             <td></td>
         </tr>
     </thead>
@@ -17,6 +39,12 @@
                     " class="btn btn-warning" 
                     onclick="return confirm('Voulez-vous supprimer cette personne ?')">
                         Supprimer
+                    </a>
+                </td>
+                <td>
+                    <a href="<?=getLinkToRoute("pdo_persons", ["id"=>       $person->id, "action"=>"update"])?>
+                    " class="btn btn-primary">
+                        Modifier
                     </a>
                 </td>
             </tr>
